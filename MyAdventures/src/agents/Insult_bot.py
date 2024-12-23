aa7d81abc42141907f7e5f8209a3290b2ae3bc7b
Luis Manuel Ocampo <luismanuel.ocampo@estudiants.urv.cat>
2024-12-23 17:49:24 +0100

    feat: InsultBot mejorado
    ahora si no lee o no encuentra el archivo pues se inicializa con valores por defecto, y se ha mejorado el tema de la ruta del archivo

--- MyAdventures/src/agents/Insult_bot.py
+++ MyAdventures/src/agents/Insult_bot.py
@@ -1,3 +1,4 @@
+import os
 import random
 from .base_agent import BaseAgent
 
@@ -7,14 +8,22 @@ random_insult = lambda n, m: random.randint(n,m)
 class InsultBot ( BaseAgent ):
     def __init__ ( self ):
         super().__init__()          # Crear Conexion
-        self.insults = [
-            'Marica',
-            'Gei',
-            'Soplanucas'
-        ]
+        self.insults = []
+        self.cargar_insultos()
+
+    def cargar_insultos ( self ):
+        base_dir  = os.path.dirname(__file__)
+        file_path = os.path.join(base_dir, '../data/insults.txt')
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                for line in file:
+                    self.insults.append(line.strip('\r\n'));
+        # Por si no lee el archivo o pasa algo raro
+        except Exception as e:
+            self.insults = ['Tonto','Feo','Baboso']
 
     def run ( self ):
-        self.mc.postToChat( self.insults[ random_insult(0, len(self.insults)-1 ) ] )
+        self.mc.postToChat( "Eres un " + self.insults[ random_insult(0, len(self.insults)-1 ) ] )
 
     def print_message ( self ):
         print("Te he insultado jaja")
