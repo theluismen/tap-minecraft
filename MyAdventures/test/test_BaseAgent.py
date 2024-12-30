c450d90def17d8787ac084e9d32ea779ab3e65e7
Luis Manuel Ocampo <luismanuel.ocampo@estudiants.urv.cat>
2024-12-30 18:44:16 +0100

    fix: El server se apaga en los tests
    no se ha provado si se apaga desde un SO Linux, solo desde windows 10

--- MyAdventures/test/test_BaseAgent.py
+++ MyAdventures/test/test_BaseAgent.py
@@ -1,6 +1,4 @@
-import os
-import time
-import subprocess
+from server_support import start_server, stop_server
 from src.agents.base_agent import BaseAgent
 
 # TEST 1: Con el servidor APAGADO, que NO se conecte
@@ -11,32 +9,10 @@ def test_BaseAgent_1():
 # TEST 2: Encendemos el servidor y verificamos que se ha conectado
 def test_BaseAgent_2():
     connected = False
-    server, server_on = server_started()
+    server, server_on = start_server()
     if server_on:
         bot = BaseAgent()
         connected = bot.connected()
-        # Apagar el servidor
-        os.system("TASKKILL /PID {0} /F".format(server.pid))
+        stop_server( server )
 
     assert connected == True
-
-def server_started():
-    process = subprocess.Popen(         # Iniciar el servidor de Minecraft
-        ['bash', "./StartServer.sh"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE, text=True
-    )
-
-    server_ready = False
-    while True:                         # Leer la salida del servidor
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
-        # Comprobar si el servidor está listo
-        if output and "Done" in output:
-            server_ready = True
-            break
-
-        time.sleep(0.1)  # Evita consumir demasiado CPU en el ciclo
-
-    return (process, server_ready)
