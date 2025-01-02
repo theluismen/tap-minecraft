70b48f2bf8e69073a802a9696aa15a95aabca0b3
Pau Valls <pau.valls@estudiant.urv.cat>
2025-01-02 13:25:32 +0100

    feat: TNT bot provisional

--- MyAdventures/src/agents/TNT_bot.py
+++ MyAdventures/src/agents/TNT_bot.py
@@ -1,11 +1,27 @@
 from .base_agent import BaseAgent
-
+import mcpi.block as Block
+import time
 class TNTBot ( BaseAgent ):
     def __init__ ( self ):
-        pass
+        super().__init__()
 
+    def perform_action(self):
+        pos = mc.player.getTilePos()
+        x, y, z = pos.x, pos.y, pos.z
+
+        # for i in range(3):
+        #     for j in range(3):
+        #         mc.setBlock(pos.x+i, pos.y+7, pos.z+i, block.TNT.id)
+        #         mc.setBlock(pos.x+j, pos.y+8, pos.z+j, block.FIRE.id)
+        print(x,y,z)
     def run ( self ):
-        print("esplotando")
+
+        self.perform_action()
+        self.mc.postToChat("Cargando tnt....")
+        self.mc.postToChat(pos.x)
+        sleep(2)
+
+        self.mc.postToChat("Cargando tnt....")
 
     def print_message ():
         print("ya esploté")
