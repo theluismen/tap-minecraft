8367a56f5de95cfc254c92a0c95503f46a978f4b
Luis Manuel Ocampo <luismanuel.ocampo@estudiants.urv.cat>
2025-01-02 14:08:32 +0100

    feat: TNT bot Acabado

--- MyAdventures/src/agents/TNT_bot.py
+++ MyAdventures/src/agents/TNT_bot.py
@@ -1,27 +1,30 @@
-from .base_agent import BaseAgent
-import mcpi.block as Block
 import time
+import mcpi.block as Block
+from .base_agent import BaseAgent
+
 class TNTBot ( BaseAgent ):
     def __init__ ( self ):
         super().__init__()
+        self.coords = [ # Un circulo de tnts
+            (-2,-2),(-1,-2),(0,-2),(1,-2),(2,-2),
+            (-2,-1),(2,-1),
+            (-2,0),(0,0),(2,0),
+            (-2,1),(2,1),
+            (-2,2),(-1,2),(0,2),(1,2),(2,2)
+        ]
 
-    def perform_action(self):
-        pos = mc.player.getTilePos()
+    def tnt( self ):
+        pos     = self.mc.player.getTilePos()
         x, y, z = pos.x, pos.y, pos.z
 
-        # for i in range(3):
-        #     for j in range(3):
-        #         mc.setBlock(pos.x+i, pos.y+7, pos.z+i, block.TNT.id)
-        #         mc.setBlock(pos.x+j, pos.y+8, pos.z+j, block.FIRE.id)
-        print(x,y,z)
+        for coord in self.coords:
+            self.mc.setBlock(x+coord[0], y+7, z+coord[1], Block.TNT.id)
+            self.mc.setBlock(x+coord[0], y+8, z+coord[1], Block.FIRE.id)
+
     def run ( self ):
+        self.mc.postToChat("Cargando tnt... Corre !")
+        time.sleep(1)
+        self.tnt()
 
-        self.perform_action()
-        self.mc.postToChat("Cargando tnt....")
-        self.mc.postToChat(pos.x)
-        sleep(2)
-
-        self.mc.postToChat("Cargando tnt....")
-
-    def print_message ():
+    def print_message ( self ):
         print("ya esploté")
